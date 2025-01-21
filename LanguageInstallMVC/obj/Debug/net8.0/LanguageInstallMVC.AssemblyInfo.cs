91ffd76cf73b2e1aa5caf180d9c246f14aac8e03
xisxus <shefain3@gmail.com>
2025-01-21 13:57:20 +0600

    new

--- LanguageInstallMVC/obj/Debug/net8.0/LanguageInstallMVC.AssemblyInfo.cs
+++ LanguageInstallMVC/obj/Debug/net8.0/LanguageInstallMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanguageInstallMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a89804c3a40399d2523d258db15e278ff3d76d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanguageInstallMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanguageInstallMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
